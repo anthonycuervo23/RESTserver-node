c75a1ed257f33bc38837becdbe15aae9ed507b89
jean cuervo <martoranito@gmail.com>
2021-08-31 23:15:46 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+ 
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+ 
+app.listen(process.env.PORT, ()=>{
+    console.log('server running in port: ', process.env.PORT);
+});
